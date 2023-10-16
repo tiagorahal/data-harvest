8023238fa032b378195c10043ded8462ddeb58d5
tiagorahal <rahal.aires@gmail.com>
2023-10-16 15:22:57 -0300

    Add parallel germ

--- scraper.rb
+++ scraper.rb
@@ -4,7 +4,7 @@ require "nokogiri"
 PokemonProduct = Struct.new(:url, :image, :name, :price) 
  
 pokemon_products = [] 
- 
+
 pages_to_scrape = ["https://scrapeme.live/shop/page/1/"] 
  
 pages_discovered = ["https://scrapeme.live/shop/page/1/"] 
@@ -24,8 +24,8 @@ while pages_to_scrape.length != 0 && i < limit do
 	document = Nokogiri::HTML(response.body) 
  
 	new_pagination_links = document 
-						.css("a.page-numbers") 
-						.map{ |a| a.attribute("href") } 
+							.css("a.page-numbers") 
+							.map{ |a| a.attribute("href") } 
  
 	new_pagination_links.each do |new_pagination_link| 
 		if !(pages_discovered.include? new_pagination_link) && !(pages_to_scrape.include? new_pagination_link) 
@@ -37,21 +37,10 @@ while pages_to_scrape.length != 0 && i < limit do
  
 	pages_discovered = pages_discovered.to_set.to_a 
  
-	html_products = document.css("li.product") 
- 
-	html_products.each do |html_product| 
-		url = html_product.css("a").first.attribute("href").value 
-		image = html_product.css("img").first.attribute("src").value 
-		name = html_product.css("h2").first.text 
-		price = html_product.css("span").first.text 
- 
-		pokemon_product = PokemonProduct.new(url, image, name, price) 
- 
-		pokemon_products.push(pokemon_product) 
-	end 
- 
 	i = i + 1 
 end 
+ 
+
  
 csv_headers = ["url", "image", "name", "price"] 
 CSV.open("output.csv", "wb", write_headers: true, headers: csv_headers) do |csv| 
