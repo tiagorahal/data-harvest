e16323733f741cc8d654893afb27006cf56561c1
tiagorahal <rahal.aires@gmail.com>
2023-10-16 15:30:52 -0300

    Refactor code to make it more readable

--- scraper.rb
+++ scraper.rb
@@ -1,50 +1,63 @@
 require "httparty" 
 require "nokogiri" 
- 
+require "parallel" 
+
 PokemonProduct = Struct.new(:url, :image, :name, :price) 
- 
-pokemon_products = [] 
 
-pages_to_scrape = ["https://scrapeme.live/shop/page/1/"] 
- 
-pages_discovered = ["https://scrapeme.live/shop/page/1/"] 
- 
-i = 0 
-limit = 5 
- 
-while pages_to_scrape.length != 0 && i < limit do 
-	page_to_scrape = pages_to_scrape.pop 
- 
-	response = HTTParty.get(page_to_scrape, { 
-		headers: { 
-			"User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" 
-		}, 
-	}) 
- 
-	document = Nokogiri::HTML(response.body) 
- 
-	new_pagination_links = document 
-							.css("a.page-numbers") 
-							.map{ |a| a.attribute("href") } 
- 
-	new_pagination_links.each do |new_pagination_link| 
-		if !(pages_discovered.include? new_pagination_link) && !(pages_to_scrape.include? new_pagination_link) 
-			pages_to_scrape.push(new_pagination_link) 
-		end 
- 
-		pages_discovered.push(new_pagination_link) 
-	end 
- 
-	pages_discovered = pages_discovered.to_set.to_a 
- 
-	i = i + 1 
-end 
- 
+class Scraper
+  USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" 
+  SEMAPHORE = Mutex.new
+  CSV_HEADERS = ["url", "image", "name", "price"]
+  PAGE_PREFIX = "https://scrapeme.live/shop/page/"
 
- 
-csv_headers = ["url", "image", "name", "price"] 
-CSV.open("output.csv", "wb", write_headers: true, headers: csv_headers) do |csv| 
-	pokemon_products.each do |pokemon_product| 
-		csv << pokemon_product 
-	end 
+  def initialize
+    @pokemon_products = []
+    @pages_to_scrape = generate_pages_to_scrape
+  end
+
+  def scrape
+    Parallel.map(@pages_to_scrape, in_threads: 4) do |page|
+      document = get_document(page)
+      process_document(document)
+    end
+    write_to_csv
+  end
+
+  private
+
+  def generate_pages_to_scrape
+    (2..48).map { |i| "#{PAGE_PREFIX}#{i}/" }
+  end
+
+  def get_document(page)
+    response = HTTParty.get(page, headers: {"User-Agent" => USER_AGENT})
+    Nokogiri::HTML(response.body)
+  end
+
+  def process_document(document)
+    document.css("li.product").each do |html_product|
+      url = html_product.css("a").first.attribute("href").value 
+      image = html_product.css("img").first.attribute("src").value 
+      name = html_product.css("h2").first.text 
+      price = html_product.css("span").first.text 
+
+      pokemon_product = PokemonProduct.new(url, image, name, price) 
+
+      SEMAPHORE.synchronize { 
+        @pokemon_products.push(pokemon_product) 
+      }
+    end
+  end
+
+  def write_to_csv
+    CSV.open("output.csv", "wb", write_headers: true, headers: CSV_HEADERS) do |csv| 
+      @pokemon_products.each do |pokemon_product| 
+        csv << pokemon_product 
+      end 
+    end
+  end
 end
+
+# use the scraper
+scraper = Scraper.new
+scraper.scrape
